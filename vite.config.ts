bbff1f128906bf93a3470d6e27faec83fc5396d4
punamth <thokarpunam14@gmail.com>
2025-06-13 20:58:17 +0545

    modified base

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/',
+  base: '/Portfolio/',
   plugins: [react(), tailwindcss()],
 })
